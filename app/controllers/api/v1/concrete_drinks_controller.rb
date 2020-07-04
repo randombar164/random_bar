4de74e449135e7448a61f91c33c6c5b33de06b98
H0R15H0 <shohori13@gmail.com>
2020-07-04 23:25:06 +0900

    make 'random returning api' prototype

--- app/controllers/api/v1/concrete_drinks_controller.rb
+++ app/controllers/api/v1/concrete_drinks_controller.rb
@@ -1,3 +1,12 @@
-class Api::V1::BaseDrinksController < Api::V1::BaseController
-  
+class Api::V1::ConcreteDrinksController < Api::V1::BaseController
+  def show
+    base_drink = BaseDrink.includes(:drink_method, :glass_type, { base_drinks_base_ingredients: [:base_ingredient, :unit] } ).find(params[:base_drink_id])
+    concrete_ingredient_ids = []
+    base_drink.base_ingredients.each do |base_ingredient|
+      concrete_ingredient_ids.push(base_ingredient.concrete_ingredients.first.id)
+    end
+    concrete_ingredients = ConcreteIngredient.includes(:handling_stores).find(concrete_ingredient_ids)
+    concrete_drink = ConcreteDrink.new(base_drink: base_drink, concrete_ingredients: concrete_ingredients)
+    render json: {concrete_drink: concrete_drink.to_json}
+  end
 end
