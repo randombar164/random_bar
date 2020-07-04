ebe54878a07ce6c9c63d759524613c944976f56f
H0R15H0 <shohori13@gmail.com>
2020-07-04 12:03:38 +0900

    add association

--- app/models/concrete_ingredient.rb
+++ app/models/concrete_ingredient.rb
@@ -1,4 +1,5 @@
 class ConcreteIngredient < ApplicationRecord
   belongs_to :base_ingredient
+  has_many :concrete_ingredients_handling_stores
   has_many :handling_stores, through: :concrete_ingredients_handling_stores
 end
