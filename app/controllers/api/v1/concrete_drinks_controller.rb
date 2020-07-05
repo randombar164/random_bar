1602e7b1600a230ded700d783717a979771c8bab
H0R15H0 <shohori13@gmail.com>
2020-07-05 23:16:14 +0900

    add query params validation method to 'random returning api'

--- app/controllers/api/v1/concrete_drinks_controller.rb
+++ app/controllers/api/v1/concrete_drinks_controller.rb
@@ -1,32 +1,29 @@
 class Api::V1::ConcreteDrinksController < Api::V1::BaseController
   def index
+    params[:base_drink_id] ||= rand(BaseDrink.count)
     base_drink = BaseDrink.includes(:drink_method, :glass_type, { base_drinks_base_ingredients: [:base_ingredient, :unit] } ).find_by(id: params[:base_drink_id])
 
     if base_drink.nil?
       response_not_found('base_drink')
-
     else
-      if params[:concrete_ingredients].present?
 
+      concrete_ingredients = []
+      concrete_ingredients_to_parse = params[:concrete_ingredients]
+
+      if base_drink.validate_params(concrete_ingredients_to_parse)
+        concrete_ingredients_to_parse.values.each do |concrete_ingredient|
+          concrete_ingredients.push(ConcreteIngredient.find(concrete_ingredient[:concrete_ingredient_id]))
+        end
       else
-        concrete_ingredients = []
         base_drink.base_ingredients.each do |base_ingredient|
           concrete_ingredients.push(base_ingredient.concrete_ingredients[rand(base_ingredient.concrete_ingredients.count)])
         end
       end
+
       concrete_drink = ConcreteDrink.new(base_drink: base_drink, concrete_ingredients: concrete_ingredients)
       render json: {concrete_drink: concrete_drink.to_json}
     end
-    # if params[:concrete_ingredients].present?
-    #   for params[:concrete_ingredients].each do |concrete_ingredient|
-    #     if params_valid?
-    #     end
-    #   end
-    # else
 
   end
 
-  def params_concrete_ingredients_valid?(concrete_ingredients)
-
-  end
 end
--- app/models/base_drink.rb
+++ app/models/base_drink.rb
@@ -3,4 +3,34 @@ class BaseDrink < ApplicationRecord
   belongs_to :glass_type
   has_many :base_drinks_base_ingredients
   has_many :base_ingredients, through: :base_drinks_base_ingredients
+
+
+  def validate_params(concrete_ingredients_to_parse)
+    return false if concrete_ingredients_to_parse.blank?
+    concrete_ingredients = concrete_ingredients_to_parse.values
+    return false if self.base_ingredients.count != concrete_ingredients.length
+    return false if !check_concrete_ingredients(concrete_ingredients)
+    return true
+  end
+
+  def check_concrete_ingredients(concrete_ingredients)
+    concrete_ingredient_ids = []
+    base_ingredient_ids = []
+    concrete_ingredients.each do |concrete_ingredient|
+      concrete_ingredient_ids.push concrete_ingredient[:concrete_ingredient_id].to_i
+      base_ingredient_ids.push concrete_ingredient[:base_ingredient_id].to_i
+    end
+    return true if self.base_ingredients.ids == base_ingredient_ids
+
+    self.base_ingredients.each_with_index do |base_ingredient, idx|
+      if base_ingredient.id == base_ingredient_ids[idx]
+        next
+      end
+      return false if base_ingredient.substitutions.nil?
+      return false unless base_ingredient.substitutions.ids.include?(base_ingredient_ids[idx])
+      return false unless BaseIngredient.find(base_ingredient_ids[idx]).concrete_ingredients.ids.include?(concrete_ingredient_ids[idx])
+    end
+    return true
+  end
+
 end
