1d47e47c637a204fb8c442b142b73f3428adc8a3
H0R15H0 <shohori13@gmail.com>
2020-07-20 21:26:00 +0900

    add method "get_base_drink_ids_from_base_ingredient_ids"

--- app/models/base_drinks_base_ingredient.rb
+++ app/models/base_drinks_base_ingredient.rb
@@ -2,4 +2,8 @@ class BaseDrinksBaseIngredient < ApplicationRecord
   belongs_to :base_ingredient
   belongs_to :base_drink
   belongs_to :unit
+
+  def self.get_base_drink_ids_from_base_ingredient_ids params_base_ingredient_ids
+    self.where(base_ingredient_id: params_base_ingredient_ids).group(:base_drink_id).having('count(base_drink_id)>=?',params_base_ingredient_ids.length).count.keys
+  end
 end
