88628935f5df2ddf95f72f6a5b932fbf59aef0eb
H0R15H0 <shohori13@gmail.com>
2020-07-22 16:17:59 +0900

    change method name

--- app/models/concrete_ingredients_handling_store.rb
+++ app/models/concrete_ingredients_handling_store.rb
@@ -2,7 +2,7 @@ class ConcreteIngredientsHandlingStore < ApplicationRecord
   belongs_to :concrete_ingredient
   belongs_to :handling_store
 
-  def self.get_base_drink_ids_from_handling_store_ids handling_store_ids
+  def self.get_concrete_ingredient_ids_from_handling_store_ids handling_store_ids
     self.where(handling_store_id: handling_store_ids).group(:concrete_ingredient_id).having('count(concrete_ingredient_id)=?',handling_store_ids.length).count.keys
   end
 end
