2dfaf49ee03ba7e5ce75fb367a24b509382f2e91
H0R15H0 <shohori13@gmail.com>
2020-08-22 16:07:30 +0900

    change production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,13 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.logger = Logger.new(STDOUT)
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
   config.log_level = :warn
 
   # Prepend all log lines with the following tags.
