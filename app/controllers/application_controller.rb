f2708a327222e2782c9fce60b276385960853bc3
H0R15H0 <shohori13@gmail.com>
2020-08-22 14:36:59 +0900

    production環境を修正
    swith port to $PORT
    
    change log to warm
    
    add config.logger
    
    change production.rb
    
    change compile to true
    
    modify production.rb
    
    change comple true to false
    
    add func to render error
    
    commentout stylesheet_link_tag
    
    webpacker.ymlを修正
    
    change extract_css true to false
    
    add config.serve_static_assets to true
    
    commented out .public_file_server.enabled
    
    remove commented out line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from StandardError do |exception|
+    # render what you want here
+    render :json => exception.to_json, :status => :unprocessable_entity
+    p "-----------------------------------------------"
+  end
 end
