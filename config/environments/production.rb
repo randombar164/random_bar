264c9f07a795720e55d5586b369aac2da0b52188
H0R15H0 <shohori13@gmail.com>
2020-08-25 19:50:56 +0900

    add config.serve_static_assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
@@ -53,11 +54,6 @@ Rails.application.configure do
   # when problems arise.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
   config.log_level = :warn
 
   # Prepend all log lines with the following tags.
