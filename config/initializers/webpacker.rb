1d84463a1715dc7e3c1aeb52ec0cd4f604fb28f4
H0R15H0 <shohori13@gmail.com>
2020-08-27 00:58:41 +0900

    fix webpacker to recieve environment variable

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,2 +1,1 @@
-Webpacker::Compiler.env['VUE_APP_TRACKING_ID'] = "VUE_APP_TRACKING_ID"#ENV['VUE_APP_TRACKING_ID']
-Webpacker::Compiler.env['TRACKING_ID'] = ENV['TRACKING_ID']
+require_relative "../webpacker_env"
---
+++ config/webpacker_env.rb
@@ -0,0 +1,1 @@
+Webpacker::Compiler.env['NODE_ENV'] = Rails.env
