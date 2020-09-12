ef2257cbbd39f57ff77debe3a90f33e6de022650
H0R15H0 <shohori13@gmail.com>
2020-09-12 20:20:21 +0900

    concrete_drinkが見つからない時404を返すようにした

--- app/controllers/api/v1/concrete_drinks_controller.rb
+++ app/controllers/api/v1/concrete_drinks_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ConcreteDrinksController < Api::V1::BaseController
     q_params = QueryParamsDecoder.new(params).decode
     q_params = QueryParamsValidator.new(q_params).validate
     concrete_drink = ConcreteDrink.new(q_params)
+    response_not_found("drink") and return if concrete_drink.nil?
     render json: {concrete_drink: concrete_drink.to_json}
   end
 
