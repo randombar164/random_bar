4bb89a742b3a8bcc50f88ab9fb80f3fcfd0e21c9
hide <nkret805worm@gmail.com>
2020-10-28 19:26:55 +0900

    いらないの削除

--- app/javascript/packs/pages/TopPage.vue
+++ app/javascript/packs/pages/TopPage.vue
@@ -4,13 +4,11 @@
     <v-container>
       <top-exp></top-exp>
     </v-container>
-    <amazon-osusume />
   </div>
 </template>
 <script>
 import TopExp from "packs/molecules/TopExp";
 import TopImg from "packs/molecules/TopImg";
-import AmazonOsusume from 'packs/molecules/AmazonOsusume';
 import IngredientsList from "packs/organisms/IngredientsList";
 import { mapState, mapActions } from 'vuex'
 
@@ -19,24 +17,10 @@ export default{
     TopExp,
     TopImg,
     IngredientsList,
-    AmazonOsusume
   },
   computed: {
-    ...mapState('drinkData',[
-      'cocktailRecipe',
-      'drinkId'
-    ])
-  },
-  created(){
-    this.getBaseIngList();
   },
   methods: {
-    ...mapActions('drinkData',[
-      'getDrink',
-      'getBaseIngList',
-      'setRecipe',
-      'removeRecipe',
-    ]),
     // async RandomGacha(){
     //   this.removeRecipe();
     //   await this.getDrink({
