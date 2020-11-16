598fdac12889a6740de4a9b71b677a74842032c8
hide <nkret805worm@gmail.com>
2020-11-16 20:21:53 +0900

    store変更

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import {drinkData} from './modules/drink';
+import createPersistedState from 'vuex-persistedstate';
+import {registerIngredients} from './modules/registerIngredients';
+import {handlingStore} from './modules/handlingStore'; 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        drinkData: drinkData
-    }
+        registerIngredients,
+        handlingStore
+    },
+    plugins: [createPersistedState({
+        key: 'gachaOptions',
+        path: [
+            'registerIngredients',
+            'handlingStore'
+        ]
+    })],
 })
