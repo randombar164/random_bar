ec2888a605f640923bd8044a7f010cd0791e09a9
H0R15H0 <shohori13@gmail.com>
2021-03-01 16:20:03 +0900

    BaseIngredientの代用も返すようにした。

--- app/controllers/api/v1/base_ingredients_controller.rb
+++ app/controllers/api/v1/base_ingredients_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::BaseIngredientsController < Api::V1::BaseController
   def index
     ids = Array(params[:ids]&.values&.map(&:to_i))
     if ids.empty?
-      base_ingredients = BaseIngredient.includes(:concrete_ingredients).all
+      base_ingredients = BaseIngredient.includes(:concrete_ingredients, :substitutions).all
     else
-      base_ingredients = BaseIngredient.includes(:concrete_ingredients).find(ids)
+      base_ingredients = BaseIngredient.includes(:concrete_ingredients, :substitutions).find(ids)
     end
-    render json: {base_ingredients: base_ingredients}, include: [:concrete_ingredients]
+    render json: {base_ingredients: base_ingredients}, include: [:concrete_ingredients, :substitutions]
   end
 
   def show
-    base_ingredient = BaseIngredient.includes(:concrete_ingredients).find(params[:id])
-    render json: {base_ingredient: base_ingredient}, include: [:concrete_ingredients]
+    base_ingredient = BaseIngredient.includes(:concrete_ingredients, :substitutions).find(params[:id])
+    render json: {base_ingredient: base_ingredient}, include: [:concrete_ingredients, :substitutions]
   end
 
   def base_drinks_count
