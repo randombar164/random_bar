3d9178bb415b9a4528e448c2501636244356d0b8
H0R15H0 <shohori13@gmail.com>
2021-03-01 16:17:53 +0900

    Event周りのモデルを関連づけた。

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  before_create do
+    self.uuid = SecureRandom.uuid
+  end
+  has_many :events_base_ingredients
+  has_many :base_ingredients, through: :events_base_ingredients
+  accepts_nested_attributes_for :events_base_ingredients, allow_destroy: true
+
 end
--- app/models/events_base_ingredient.rb
+++ app/models/events_base_ingredient.rb
@@ -1,2 +1,4 @@
 class EventsBaseIngredient < ApplicationRecord
+  belongs_to :event
+  belongs_to :base_ingredient
 end
