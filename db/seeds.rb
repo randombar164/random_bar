9e7e3ddc7d38ddcf7e773bcbe2a3148b7468175f
H0R15H0 <shohori13@gmail.com>
2021-04-10 19:54:04 +0900

    Event材料を任せる人のカラムを追加した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+e = Event.create(name: "sample event")
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 3)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 1)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 38)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 7)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 21)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 20)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 74)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 4)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 15)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 22)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 27)
+EventsBaseIngredient.create(event_id: e.id, base_ingredient_id: 36)
